9a2dd6f5902c323ff1d020460904ca08f693d816
israel <israel.bejarano.a@gmail.com>
2019-11-23 12:33:32 +0100

    monstrar mapa en universidad Canadá

--- src/app/components/mapa/mapa.component.ts
+++ src/app/components/mapa/mapa.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-mapa',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapaComponent implements OnInit {
 
+  mapa: mapboxgl.Map;
+
   constructor() { }
 
   ngOnInit() {
+    this.crearMapa();
+  }
+
+  crearMapa() {
+    (mapboxgl as any).accessToken = 'pk.eyJ1IjoiaXNyYWVsYmEiLCJhIjoiY2p6cjFvYmZlMHk3azNrbG0yODdjcTBkMCJ9.rQHGRKKilUn8zDx6vyijlA';
+    this.mapa = new mapboxgl.Map({
+      container: 'mapa',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-75.75512993582937, 45.349977429009954],
+      zoom: 15.8
+    });
   }
 
 }
