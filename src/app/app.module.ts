f9a947a49c52d5802a9a6dc49217fe3dddcb423d
israel <israel.bejarano.a@gmail.com>
2019-11-24 11:35:25 +0100

    configurar sockets lib para conectar al server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// sockets
+import { SocketIoModule } from 'ngx-socket-io';
+
 import { AppComponent } from './app.component';
 import { MapaComponent } from './components/mapa/mapa.component';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,8 @@ import { MapaComponent } from './components/mapa/mapa.component';
     MapaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SocketIoModule.forRoot(environment.socketConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
